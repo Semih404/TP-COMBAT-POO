add643ab339def5f77414d0cd572a2ddf3d872fa
Serhatman <119849836+Serhatman@users.noreply.github.com>
2023-02-10 16:05:21 +0100

    Update autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -1,1 +1,28 @@
+<?php
+class Autoloader{
+
+/**
+ * Enregistre notre autoloader
+ */
+static function register(){
+    spl_autoload_register(array(__CLASS__, 'autoload'));
+}
+
+/**
+ * Inclue le fichier correspondant à notre classe
+ * @param $class string Le nom de la classe à charger
+ */
+static function autoload($class){
+    require 'class/' . $class . '.php';
+}
+
+}
+
+// Enregistre notre autoloader
+Autoloader::register();
+
+// Instanciation des classes
+$archer = new Archer();
+$guerrier = new Guerrier();
+$mage = new Mage();
 
