3183c59e5e8bac03f22b31e5179cfbb99604b122
Serhat <119849836+Serhatman@users.noreply.github.com>
2023-02-14 13:18:12 +0100

    Update HeroesManager.php

--- classes/HeroesManager.php
+++ classes/HeroesManager.php
@@ -2,108 +2,52 @@
 session_start();
 include '../config/db.php';
 
+$isSuccess = false;
 
-if (isset($_POST['nom']) && !empty($_POST['nom'] ))
-{
-    $isSuccess;
-    if(isset($_POST['nom']) && !empty($_POST['nom'])) {
-        
-        try {
-            // Test si le username existe
-            $nomStmnt = $db->prepare('SELECT * FROM heros WHERE nom = ?');
-            $nomStmnt->execute([
-                $_POST['nom']
-            ]);
+$nom = null;
 
-            $nom = $nomStmnt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Une erreur est survenue : " . $e->getMessage();
-        }
-    }};
-        if(empty($nom)) {
-
-            try {
-            // SI l'utilisateur n'existe pas 
-            // On le crée
-                $nomStmnt = $db->prepare('INSERT INTO heros(nom) VALUES (?)');
-                $isSuccess = $nomStmnt->execute([
-                    $_POST['nom'],
-                ]);
-            } catch (PDOException $e) {
-                echo "Une erreur est survenue lors de l'insertion de l'utilisateur : " . $e->getMessage();
-            }
-        }
-
-
-        if (isset($_POST['classe']) && !empty($_POST['classe'] ))
-{
-    $isSuccess;
-    if(isset($_POST['classe']) && !empty($_POST['classe'])) {
-        
-        try {
-            // Test si le username existe
-            $classeStmnt = $db->prepare('SELECT * FROM heros WHERE classe = ?');
-            $classeStmnt->execute([
-                $_POST['classe']
-            ]);
-
-            $classe = $classeStmnt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Une erreur est survenue : " . $e->getMessage();
-        }
-    }};
-        if(empty($classe)) {
-
-            try {
-            // SI l'utilisateur n'existe pas 
-            // On le crée
-                $classeStmnt = $db->prepare('INSERT INTO heros(classe) VALUES (?)');
-                $isSuccess = $classeStmnt->execute([
-                    $_POST['classe'],
-                ]);
-            } catch (PDOException $e) {
-                echo "Une erreur est survenue lors de l'insertion de l'utilisateur : " . $e->getMessage();
-            }
-        }
-
-                if (isset($_POST['classe']) && !empty($_POST['classe'] ))
-{
-    $isSuccess;
-    if(isset($_POST['classe']) && !empty($_POST['classe'])) {
-        
-        try {
-            // Test si le username existe
-            $classeStmnt = $db->prepare('SELECT * FROM heros WHERE classe = ?');
-            $classeStmnt->execute([
-                $_POST['classe']
-            ]);
-
-            $classe = $classeStmnt->fetch(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "Une erreur est survenue : " . $e->getMessage();
-        }
-    }};
-        if(empty($classe)) {
-
-            try {
-            // SI l'utilisateur n'existe pas 
-            // On le crée
-                $classeStmnt = $db->prepare('INSERT INTO heros(classe) VALUES (?)');
-                $isSuccess = $classeStmnt->execute([
-                    $_POST['classe'],
-                ]);
-            } catch (PDOException $e) {
-                echo "Une erreur est survenue lors de l'insertion de l'utilisateur : " . $e->getMessage();
-            }
-        }
-        
-        $vivantStmnt = $db->prepare('INSERT INTO heros(vivant) VALUES (?)');
-        $isSuccess = $vivantStmnt->execute([
-            true,]);
-
+if(isset($_POST['nom']) && !empty($_POST['nom']) && filter_var($_POST['nom'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z]+$/")))) {
+    if (isset($_POST['nom'])) {
+        $nomStmnt = $db->prepare('SELECT * FROM heros WHERE nom = ?');
+        $nomStmnt->execute([$_POST['nom']]);
+        $nom = $nomStmnt->fetch(PDO::FETCH_ASSOC);
         $_SESSION['classe'] = $_POST['classe'];
         $_SESSION['nom'] = $_POST['nom'];
         $_SESSION['vivant'] = true;
-        
         header('Location:../fight.php');
-        ?>
+    }
+}
+
+if(empty($nom)) {
+
+    try {
+
+        if (isset($_POST['classe'])) {
+            $nomStmnt = $db->prepare('INSERT INTO heros(nom , classe, vivant) VALUES (?,?,?)');
+            $isSuccess = $nomStmnt->execute([
+                $_POST['nom'],
+                $_POST['classe'],
+                true
+            ]);
+        } else {
+            // Gérer l'erreur de données manquantes ici
+        }
+
+    } catch (PDOException $e) {
+        error_log("Erreur lors de l'insertion de l'utilisateur : " . $e->getMessage());
+        echo "Une erreur est survenue lors de l'insertion de l'utilisateur : " . $e->getMessage();
+    }
+
+}
+
+if ($isSuccess) {
+    $_SESSION['classe'] = $_POST['classe'];
+    $_SESSION['nom'] = $_POST['nom'];
+    $_SESSION['vivant'] = true;
+    header('Location:../fight.php');
+} else {
+    // Gérer l'erreur d'insertion ici
+    if ($nomStmnt->errorInfo()[1] == 1062) {
+        echo "Erreur : cet utilisateur existe déjà";
+    }
+}
