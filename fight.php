1d6ca1d150858eb8f78ada7c594a4752326a6ff1
Serhat <119849836+Serhatman@users.noreply.github.com>
2023-02-14 13:58:57 +0100

    Update fight.php

--- fight.php
+++ fight.php
@@ -1,3 +1,28 @@
 <?php
+session_start();
 
+require_once('config/autoload.php');
+
+require_once('classes/Archer.php');
+require_once('classes/Guerrier.php');
+require_once('classes/Mage.php');
+
+$archer = new Archer();
+$guerrier = new Guerrier();
+$mage = new Mage();
+$classe;
+if($_SESSION['classe']== "Archer"){
+    $classe = $archer->toString();
+    echo '<img src="' . $archer->face . '">';
+} elseif($_SESSION['classe']== "Guerrier"){
+    $classe = $guerrier->toString();
+    echo '<img src="' . $guerrier->face . '">';
+} elseif($_SESSION['classe']== "Mage"){
+    $classe = $mage->toString();
+    echo '<img src="' . $mage->face . '">';
+};
+
+$joueur =  $_SESSION['nom'] . $classe;
+
+echo $joueur;
 ?>
