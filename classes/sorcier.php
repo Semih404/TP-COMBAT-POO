2ffa6e9c5da634e843ea2fbe1fa350212124d076
Semih404 <119291380+Semih404@users.noreply.github.com>
2023-02-14 17:11:38 +0100

    Update sorcier.php

--- classes/sorcier.php
+++ classes/sorcier.php
@@ -12,7 +12,7 @@ class Sorcier extends Monstre{
     public function competence1($p){
         return "Ton adversaire attaque avec une magie de 20, tu subit " . $p->perdrePVbrut(20) . " dégats";
     }
-    public function competence2(){
+    public function competence2($p){
         $this->pv = $this->pv+30;
         return "Ton adversaire se soigne 30 pv";
     }
