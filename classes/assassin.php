5f29665429bb793f7bb42f7792b0c8e413ab9d5b
Semih404 <119291380+Semih404@users.noreply.github.com>
2023-02-14 13:53:28 +0100

    Update and rename fantassin.php to assassin.php

---
+++ classes/assassin.php
@@ -0,0 +1,16 @@
+<?php
+class assassin extends monstre{
+    public $pv = 100;
+    public $atk = 12;
+    public $arm = 1;
+    public $mana = 10;
+    public $regman = 1;
+    public function competence1($p){
+        $r= random_int(1,6);
+        return "Ton adversaire attaque avec une dague empoisonnée, tu subis " . $p->perdrePV($this->atk) . " dégats + ". $p->perdrePVbrut($r) . " dégats de poison";
+    }
+    public function competence2(){
+        return "Ton adversaire augmente son attaque de " . $this->atk=$this->atk+3;
+    }
+}
+?>
