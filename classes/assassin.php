bfe64b61e7593d79b343760c6cd48f199284c2d7
Semih404 <119291380+Semih404@users.noreply.github.com>
2023-02-14 15:00:29 +0100

    Update assassin.php

--- classes/assassin.php
+++ classes/assassin.php
@@ -3,8 +3,8 @@ class Assassin extends Monstre{
     public $nom = 'Assassin';
     public $face = "images/assassin.gif";
     public $pv = 100;
-    public $atk = 8;
-    public $arm = 6;
+    public $atk = 12;
+    public $arm = 1;
     public $mana = 10;
     public $regman = 1;
     public function competence1($p){
