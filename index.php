297c1f69abd43149adbefeeac208f8a76bf50217
Semih404 <119291380+Semih404@users.noreply.github.com>
2023-03-02 11:21:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Ultimate Fighting Game</title>
 </head>
 <body style="background-image: url('http://twistedsifter.com/wp-content/uploads/2013/05/animated-gifs-of-fighting-game-backgrounds-45.gif'); background-repeat: no-repeat;
    background-position: center center;
